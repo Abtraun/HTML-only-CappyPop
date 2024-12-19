fda05e1338317df0b092f824ebcafccfa03cbfcd
abtraun <abgail2304@gmail.com>
2024-12-19 21:51:15 +0700

    payment modal added

--- js/admin-script.js
+++ js/admin-script.js
@@ -5,3 +5,26 @@ for (var i = 0; i < arrow.length; i++) {
  arrowParent.classList.toggle("showMenu");
   });
 }
+
+// Get the modal and the button that opens it
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("openModalBtn");
+var closeBtn = document.getElementsByClassName("close-btn")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+closeBtn.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
