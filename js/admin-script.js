a3892751c8c0faf326341c267370bf282e2ec238
abtraun <abgail2304@gmail.com>
2025-01-17 17:07:40 +0700

    multiple

--- js/admin-script.js
+++ js/admin-script.js
@@ -28,3 +28,40 @@ window.onclick = function(event) {
     }
 }
 
+
+// Get the select element and the list where selected options will be displayed
+const sizeSelect = document.getElementById("size-select");
+const selectedList = document.getElementById("selected-list");
+
+// Event listener for when the user selects/deselects options
+sizeSelect.addEventListener("change", updateSelectedList);
+
+// Function to update the list of selected items
+function updateSelectedList() {
+    // Clear the current list
+    selectedList.innerHTML = '';
+
+    // Get the selected options
+    const selectedOptions = Array.from(sizeSelect.selectedOptions);
+
+    // Add each selected option to the displayed list
+    selectedOptions.forEach(option => {
+        const listItem = document.createElement("li");
+        listItem.textContent = option.value;
+
+        // Add a remove button to each selected item
+        const removeBtn = document.createElement("span");
+        removeBtn.textContent = "X";
+        removeBtn.classList.add("remove-item");
+        removeBtn.onclick = () => removeSelectedOption(option);
+
+        listItem.appendChild(removeBtn);
+        selectedList.appendChild(listItem);
+    });
+}
+
+// Function to remove an option from the selection list
+function removeSelectedOption(option) {
+    option.selected = false;  // Deselect the option in the dropdown
+    updateSelectedList();  // Update the displayed list
+}
